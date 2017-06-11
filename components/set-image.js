18588e365987542ec6157356a6a2a9e2857910fa
mkopar <matejkopar@gmail.com>
2017-06-11 16:07:24 +0200

    framework set up

---
+++ components/set-image.js
@@ -0,0 +1,170 @@
+/* global AFRAME */
+
+/**
+ * Component that listens to an event, fades out an entity, swaps the texture, and fades it
+ * back in.
+ */
+AFRAME.registerComponent('set-image-forward', {
+  schema: {
+    on: {type: 'string'},
+    target: {type: 'selector'},
+    src: {type: 'string'},
+    dur: {type: 'number', default: 300}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+
+    this.setupFadeAnimation();
+
+    el.addEventListener(data.on, function () {
+      // Fade out image.
+      data.target.emit('set-image-fade');
+      // Wait for fade to complete.
+      setTimeout(function () {
+        // Set image.
+        // read #pic_num
+        var pic_num = (parseInt(document.querySelector('#pic_num').value) + 1) % 10
+        var phi = parseInt(document.querySelector('#pic' + pic_num + "_phi").value)
+        data.target.setAttribute('material', 'src', '#pic' + pic_num);
+        data.target.setAttribute('phi-start', phi)
+        // set new #pic_num
+        document.querySelector('#pic_num').value = pic_num
+      }, data.dur);
+    });
+  },
+
+  /**
+   * Setup fade-in + fade-out.
+   */
+  setupFadeAnimation: function () {
+    var data = this.data;
+    var targetEl = this.data.target;
+
+    // Only set up once.
+    if (targetEl.dataset.setImageFadeSetup) { return; }
+    targetEl.dataset.setImageFadeSetup = true;
+
+    // Create animation.
+    targetEl.setAttribute('animation__fade', {
+      property: 'material.color',
+      startEvents: 'set-image-fade',
+      dir: 'alternate',
+      dur: data.dur,
+      from: '#FFF',
+      to: '#000'
+    });
+  }
+});
+
+AFRAME.registerComponent('set-image-back', {
+  schema: {
+    on: {type: 'string'},
+    target: {type: 'selector'},
+    src: {type: 'string'},
+    dur: {type: 'number', default: 300}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+
+    this.setupFadeAnimation();
+
+    el.addEventListener(data.on, function () {
+      // Fade out image.
+      data.target.emit('set-image-fade');
+      // Wait for fade to complete.
+      setTimeout(function () {
+        // Set image.
+        // read #pic_num
+        var pic_num = (parseInt(document.querySelector('#pic_num').value) - 1) % 10
+        if (pic_num < 0) {
+          pic_num = 9
+        }
+        var phi = parseInt(document.querySelector('#pic' + pic_num + "_phi").value)
+        data.target.setAttribute('material', 'src', '#pic' + pic_num);
+        data.target.setAttribute('phi-start', phi)
+        // set new #pic_num
+        document.querySelector('#pic_num').value = pic_num
+      }, data.dur);
+    });
+  },
+
+  /**
+   * Setup fade-in + fade-out.
+   */
+  setupFadeAnimation: function () {
+    var data = this.data;
+    var targetEl = this.data.target;
+
+    // Only set up once.
+    if (targetEl.dataset.setImageFadeSetup) { return; }
+    targetEl.dataset.setImageFadeSetup = true;
+
+    // Create animation.
+    targetEl.setAttribute('animation__fade', {
+      property: 'material.color',
+      startEvents: 'set-image-fade',
+      dir: 'alternate',
+      dur: data.dur,
+      from: '#FFF',
+      to: '#000'
+    });
+  }
+});
+
+AFRAME.registerComponent('set-image-home', {
+  schema: {
+    on: {type: 'string'},
+    target: {type: 'selector'},
+    src: {type: 'string'},
+    dur: {type: 'number', default: 300}
+  },
+
+  init: function () {
+    var data = this.data;
+    var el = this.el;
+
+    this.setupFadeAnimation();
+
+    el.addEventListener(data.on, function () {
+      // Fade out image.
+      data.target.emit('set-image-fade');
+      // Wait for fade to complete.
+      setTimeout(function () {
+        // Set image.
+        // read #pic_num
+        var pic_num = 0
+        var phi = parseInt(document.querySelector('#pic' + pic_num + "_phi").value)
+        data.target.setAttribute('material', 'src', '#pic' + pic_num);
+        data.target.setAttribute('phi-start', phi)
+        // set new #pic_num
+        document.querySelector('#pic_num').value = pic_num
+      }, data.dur);
+    });
+  },
+
+  /**
+   * Setup fade-in + fade-out.
+   */
+  setupFadeAnimation: function () {
+    var data = this.data;
+    var targetEl = this.data.target;
+
+    // Only set up once.
+    if (targetEl.dataset.setImageFadeSetup) { return; }
+    targetEl.dataset.setImageFadeSetup = true;
+
+    // Create animation.
+    targetEl.setAttribute('animation__fade', {
+      property: 'material.color',
+      startEvents: 'set-image-fade',
+      dir: 'alternate',
+      dur: data.dur,
+      from: '#FFF',
+      to: '#000'
+    });
+  }
+});
